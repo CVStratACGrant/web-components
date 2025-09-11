9d9d1a8bfe4ec105b9828986b6556c69d89c339c
CV Strategies <cvstrategies@macbookpro.lan>
2025-09-10 17:30:17 -0700

    remove testing mode

--- indio-water-authority-bill-estimator/script.js
+++ indio-water-authority-bill-estimator/script.js
@@ -21,16 +21,16 @@ class IndioWaterAuthorityBillEstimator extends HTMLElement {
             // await this.loadScript('./water-core.js');
 
             // Puppeteer
-            await this.loadScript('indio-water-authority-bill-estimator/water-rates-and-charges.js');
-            await this.loadScript('indio-water-authority-bill-estimator/water-view.js');
-            await this.loadScript('indio-water-authority-bill-estimator/water-functions.js');
-            await this.loadScript('indio-water-authority-bill-estimator/water-core.js');
+            // await this.loadScript('indio-water-authority-bill-estimator/water-rates-and-charges.js');
+            // await this.loadScript('indio-water-authority-bill-estimator/water-view.js');
+            // await this.loadScript('indio-water-authority-bill-estimator/water-functions.js');
+            // await this.loadScript('indio-water-authority-bill-estimator/water-core.js');
             
             // Live
-            // await this.loadScript('https://cvstratacgrant.github.io/web-components/indio-water-authority-bill-estimator/water-rates-and-charges.js');
-            // await this.loadScript('https://cvstratacgrant.github.io/web-components/indio-water-authority-bill-estimator/water-view.js');
-            // await this.loadScript('https://cvstratacgrant.github.io/web-components/indio-water-authority-bill-estimator/water-functions.js');
-            // await this.loadScript('https://cvstratacgrant.github.io/web-components/indio-water-authority-bill-estimator/water-core.js');
+            await this.loadScript('https://cvstratacgrant.github.io/web-components/indio-water-authority-bill-estimator/water-rates-and-charges.js');
+            await this.loadScript('https://cvstratacgrant.github.io/web-components/indio-water-authority-bill-estimator/water-view.js');
+            await this.loadScript('https://cvstratacgrant.github.io/web-components/indio-water-authority-bill-estimator/water-functions.js');
+            await this.loadScript('https://cvstratacgrant.github.io/web-components/indio-water-authority-bill-estimator/water-core.js');
         } catch (error) {
             console.error('Failed to load scripts:', error);
         }
